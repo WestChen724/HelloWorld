9d89a0210522b21d7f9d1fa83da437f9fac00a92
Westchen724 <1103791585@qq.com>
2019-10-09 17:27:02 +0800

    hiclass

---
+++ ArrayCopy.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class ArrayCopy{
+	public static void main(String[] args){
+		int[] array1 = {1,2,3,4,5,6,7,8,9,10};
+		int[] array2 = {0,0,0,0,0,0,0,0,0,0};
+		System.arraycopy(array1,0,array2,0,6);
+		System.out.print("array2:");
+		for (int i = 0; i < array2.length ; i++){
+			System.out.print(array2[i] + " ");
+		}
+	}
+}
